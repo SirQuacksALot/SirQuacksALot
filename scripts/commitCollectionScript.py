1ba6071b23c61d5b09a970438b767bb37fec408c
sebastian <108793124+SirQuacksALot@users.noreply.github.com>
2025-01-14 11:21:59 +0100

    Update commitCollectionScript.py

--- scripts/commitCollectionScript.py
+++ scripts/commitCollectionScript.py
@@ -3,6 +3,7 @@ import json
 import requests
 
 def collect_commit_stats():
+    print(f"{os.getenv('GITHUB_TOKEN')}")
     # Load repositories from GitHub API
     headers = {"Authorization": f"Bearer {os.getenv('GITHUB_TOKEN')}"}
     response = requests.get("https://api.github.com/users/SirQuacksALot/repos?per_page=100", headers=headers)
